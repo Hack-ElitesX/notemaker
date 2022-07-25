8688358249dcf97254aa140bff9ca96be26d5142
Jaishree2310 <jaishrees23102001@gmail.com>
2022-07-25 10:11:49 +0530

    feat: initial project template

---
+++ notemakerapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect, render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from bootstrap_modal_forms.mixins import PassRequestMixin
+from django.contrib import messages
+from django.db.models import Sum
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView, ListView
+
+from . import models
+import operator
+import itertools
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import authenticate, logout
+from django.contrib import auth, messages
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+
+def home(request):
+    return render(request, 'home.html')

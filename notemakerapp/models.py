f73646e4e5892959179ad42f2a0b6e1256d8ad1a
Michelle1305 <sagarmemane_iot_2020@ltce.in>
2022-08-15 12:40:18 +0530

    Added Collections functionality

---
+++ notemakerapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Collections(models.Model):
+    username = models.CharField(max_length=50,default='User')
+    title = models.CharField(max_length=15)
+    desc = models.TextField()
+    
+    def __str__(self):
+        return self.username + str('_') + self.title
+    
